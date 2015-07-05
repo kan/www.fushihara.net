2bb223d943b24a310200259c213d5258ddf05253
Kan Fushihara <kan.fushihara@gmail.com>
2015-07-05 16:34:21 +0900

    index.htmlをリニューアル。gulpでの開発支援の仕組みも追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('default', ['browser-sync']);
 
@@ -10,5 +11,22 @@ gulp.task('browser-sync', function() {
             index: 'index.html'
         }
     });
+
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('css/*.css').on('change', browserSync.reload);
 });
 
+gulp.task('minify-css', function() {
+    return gulp.src('css/*.css')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['minify-css'], function() {
+    gulp.src('*.html')
+        .pipe(gulp.dest('dist'));
+    gulp.src('js/*.js')
+        .pipe(gulp.dest('dist/js'));
+    gulp.src('img/*.*')
+        .pipe(gulp.dest('dist/img'));
+});
