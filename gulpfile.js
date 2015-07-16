ee388dfecba372c54d45063fa48be865262adb33
Kan Fushihara <kan.fushihara@gmail.com>
2015-07-16 15:24:03 +0900

    gulp-load-pluginsでrequire地獄を解消
    gulp helpでtask一覧が出るようにしてみた

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,18 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
-var minifyCss = require('gulp-minify-css');
-var shell = require('gulp-shell');
-var gzip = require('gulp-gzip');
-var bower = require('main-bower-files');
-var concat = require('gulp-concat');
-var react = require('gulp-react');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var filter = require('gulp-filter');
-var sourcemaps = require('gulp-sourcemaps');
-var sass = require('gulp-sass');
+
 var browserify = require('browserify');
 var reactify = require('reactify');
 var debowerify = require('debowerify');
+
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var debug = require('gulp-debug');
+
 
 gulp.task('default', ['browser-sync']);
+
+gulp.task('help', $.taskListing.withFilters(null,'default'));
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -37,22 +29,22 @@ gulp.task('browser-sync', function() {
 
 gulp.task('sass', function() {
     return gulp.src('src/**/*.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('./'))
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({outputStyle: 'compressed'}))
+        .pipe($.rename({ suffix: '.min' }))
+        .pipe($.sourcemaps.write('./'))
         .pipe(gulp.dest('src'));
 });
 
 gulp.task('minify-css', ['sass'], function() {
     return gulp.src('src/**/*.css')
-        .pipe(plumber())
-        .pipe(filter(['**/*.css', '!**/*.min.css']))
-        .pipe(sourcemaps.init())
-        .pipe(minifyCss())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('./'))
+        .pipe($.plumber())
+        .pipe($.filter(['**/*.css', '!**/*.min.css']))
+        .pipe($.sourcemaps.init())
+        .pipe($.minifyCss())
+        .pipe($.rename({ suffix: '.min' }))
+        .pipe($.sourcemaps.write('./'))
         .pipe(gulp.dest('src'));
 });
 
@@ -62,25 +54,25 @@ gulp.task('script', function() {
         transform: [reactify, debowerify],
         debug: true
     }).bundle()
-        .pipe(plumber())
+        .pipe($.plumber())
         .pipe(source('app.js'))
         .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
+        .pipe($.sourcemaps.init({ loadMaps: true }))
+        .pipe($.uglify())
+        .pipe($.sourcemaps.write('./'))
         .pipe(gulp.dest('src/js'));
 });
 
 gulp.task('build', ['script','minify-css'], function() {
     gulp.src('src/**/*.*')
-        .pipe(plumber())
-        .pipe(filter(['**/*', '!**/*.jsx', '!**/*.scss']))
+        .pipe($.plumber())
+        .pipe($.filter(['**/*', '!**/*.jsx', '!**/*.scss']))
         .pipe(gulp.dest('dist'))
-        .pipe(gzip())
+        .pipe($.gzip())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('release', shell.task([
+gulp.task('release', $.shell.task([
     'ssh fushihara.net "cd /var/www; git pull"',
     'open https://fushihara.net/'
 ]));
